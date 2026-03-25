a8e2a4e233cae6b1eff092043b9fd4590b742ba4
Bhumika0403 <bhumikavarshney33 @gmail.com>
2026-03-25 23:56:11 +0530

    Added folder_04_css

---
+++ folder_04_css/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header_module.css'
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h3 className={styles.logo}>Header</h3>
+      <button className={styles.btn}>Login</button>
+    </div>
+  )
+}
+
+export default Header
